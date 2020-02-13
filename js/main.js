b60b4b709cd6fed374ceb09a648c867ca1bd5994
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2020-02-13 13:26:09 +0300

    fix edge bug

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const videoContent = document.querySelector(".video-content");
 const formContent = document.querySelector('.form')
 const grayBackground = document.querySelector(".gray-background");
 
-const startVideo = state => {
+function startVideo (state) {
    videoContent.style.display = state;
    grayBackground.style.display = state;
 };
 
-const popoutForm = state => {
+function popoutForm (state) {
    window.scrollTo(pageXOffset, 0)
    formContent.style.display = state;
    grayBackground.style.display = state;
